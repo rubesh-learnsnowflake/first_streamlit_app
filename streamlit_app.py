1c7b13269869cc868a57bcceefab37aefcb1cb98
Rubesh Murthy G <114987870+rubesh-learnsnowflake@users.noreply.github.com>
2022-10-10 12:05:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('Shed Red Blood')
+streamlit.header('The Vengeance entered the Grand Palace')
+streamlit.text('Cowardness was not in their dictionary')
+streamlit.text('Bravery with Honesty was the weapon used against Vengeance and Betrayal')

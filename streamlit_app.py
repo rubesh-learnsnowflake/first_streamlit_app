0e84122d0860ae2c8cc13e09a99c29bb056c839d
Rubesh Murthy G <114987870+rubesh-learnsnowflake@users.noreply.github.com>
2022-10-17 15:39:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+#import streamlit
 
 streamlit.title('Shed Red Blood')
 streamlit.header('The Vengeance entered the Grand Palace')
@@ -6,7 +12,7 @@ streamlit.text('Cowardness was not in their dictionary')
 streamlit.text('Bravery forged with Honesty was the weapon used against Vengeance and Betrayal')
 streamlit.text('🍌🥭 War left both the states suffer from hunger and illness�🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,7 +31,7 @@ streamlit.header ('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
 
@@ -34,7 +40,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
